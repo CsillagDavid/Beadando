8636ca46d560ae1583dfb185c22a3c618354ea24
JungTamas <47351878+JungTamas@users.noreply.github.com>
2020-03-15 17:54:56 +0100

    sql

---
+++ SQL Scripts/Insert_Felhasz.sql
@@ -0,0 +1,9 @@
+USE wtDB
+GO
+
+--Felhasználók tábla feltöltése
+INSERT INTO dbo.Felhasznalok VALUES (1,'admin','Asdasd1111','admin@wtdb.com','0')
+INSERT INTO dbo.Felhasznalok VALUES (2,'jungt','Asdasd1111','jungt@wtdb.com','7')
+INSERT INTO dbo.Felhasznalok VALUES (3,'csdavid','Asdasd1111','csdavid@wtdb.com','5')
+
+--Munkaidõ tábla feltöltése
