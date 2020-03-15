1dc913e69278315f817e9e2f596d8704e18a7e76
JungTamas <47351878+JungTamas@users.noreply.github.com>
2020-03-15 16:55:30 +0100

    sql test

--- Beadando/wtForm.vb
+++ Beadando/wtForm.vb
@@ -1,3 +1,7 @@
 ﻿Public Class wtForm
+    Private Sub wtForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim db As New Database
+        db.sqlconnection()
 
+    End Sub
 End Class
