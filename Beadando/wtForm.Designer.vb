0cdd28692502ab4c51e432ea0cdb4ee86ff1b7a2
Jung Tamás <jung.tamas94@gmail.com>
2020-03-22 17:22:58 +0100

    mai munka

--- Beadando/wtForm.Designer.vb
+++ Beadando/wtForm.Designer.vb
@@ -24,16 +24,21 @@ Partial Class wtForm
     Private Sub InitializeComponent()
         Me.lblFelhasznalok = New System.Windows.Forms.Label()
         Me.ltbFelhasznalok = New System.Windows.Forms.ListBox()
-        Me.dgvFelhasznalok = New System.Windows.Forms.DataGridView()
+        Me.dgvTabla = New System.Windows.Forms.DataGridView()
         Me.btnFelhasznalok = New System.Windows.Forms.Button()
-        Me.btnMunkaidő = New System.Windows.Forms.Button()
-        CType(Me.dgvFelhasznalok, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.btnMunkaidoleker = New System.Windows.Forms.Button()
+        Me.btnMentes = New System.Windows.Forms.Button()
+        Me.btnTorles = New System.Windows.Forms.Button()
+        Me.btnMunkaidoossz = New System.Windows.Forms.Button()
+        Me.txtMunkaidoOsszes = New System.Windows.Forms.TextBox()
+        Me.lblMunkaidoOsszes = New System.Windows.Forms.Label()
+        CType(Me.dgvTabla, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'lblFelhasznalok
         '
         Me.lblFelhasznalok.AutoSize = True
-        Me.lblFelhasznalok.Location = New System.Drawing.Point(12, 9)
+        Me.lblFelhasznalok.Location = New System.Drawing.Point(12, 119)
         Me.lblFelhasznalok.Name = "lblFelhasznalok"
         Me.lblFelhasznalok.Size = New System.Drawing.Size(72, 13)
         Me.lblFelhasznalok.TabIndex = 0
@@ -42,52 +47,101 @@ Partial Class wtForm
         'ltbFelhasznalok
         '
         Me.ltbFelhasznalok.FormattingEnabled = True
-        Me.ltbFelhasznalok.Location = New System.Drawing.Point(15, 35)
+        Me.ltbFelhasznalok.Location = New System.Drawing.Point(15, 135)
         Me.ltbFelhasznalok.Name = "ltbFelhasznalok"
         Me.ltbFelhasznalok.Size = New System.Drawing.Size(156, 160)
         Me.ltbFelhasznalok.TabIndex = 1
         '
-        'dgvFelhasznalok
+        'dgvTabla
         '
-        Me.dgvFelhasznalok.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
-        Me.dgvFelhasznalok.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
-        Me.dgvFelhasznalok.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvFelhasznalok.Location = New System.Drawing.Point(186, 9)
-        Me.dgvFelhasznalok.Name = "dgvFelhasznalok"
-        Me.dgvFelhasznalok.Size = New System.Drawing.Size(765, 517)
-        Me.dgvFelhasznalok.TabIndex = 4
+        Me.dgvTabla.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dgvTabla.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
+        Me.dgvTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvTabla.Location = New System.Drawing.Point(186, 9)
+        Me.dgvTabla.Name = "dgvTabla"
+        Me.dgvTabla.Size = New System.Drawing.Size(765, 526)
+        Me.dgvTabla.TabIndex = 4
         '
         'btnFelhasznalok
         '
-        Me.btnFelhasznalok.Location = New System.Drawing.Point(15, 286)
+        Me.btnFelhasznalok.Location = New System.Drawing.Point(15, 301)
         Me.btnFelhasznalok.Name = "btnFelhasznalok"
-        Me.btnFelhasznalok.Size = New System.Drawing.Size(85, 23)
+        Me.btnFelhasznalok.Size = New System.Drawing.Size(156, 23)
         Me.btnFelhasznalok.TabIndex = 5
-        Me.btnFelhasznalok.Text = "Felhasználók"
+        Me.btnFelhasznalok.Text = "Felhasználók adatai"
         Me.btnFelhasznalok.UseVisualStyleBackColor = True
         '
-        'btnMunkaidő
+        'btnMunkaidoleker
         '
-        Me.btnMunkaidő.Location = New System.Drawing.Point(15, 211)
-        Me.btnMunkaidő.Name = "btnMunkaidő"
-        Me.btnMunkaidő.Size = New System.Drawing.Size(75, 46)
-        Me.btnMunkaidő.TabIndex = 6
-        Me.btnMunkaidő.Text = "Munkaidő lekérdezése"
-        Me.btnMunkaidő.UseVisualStyleBackColor = True
+        Me.btnMunkaidoleker.Location = New System.Drawing.Point(12, 70)
+        Me.btnMunkaidoleker.Name = "btnMunkaidoleker"
+        Me.btnMunkaidoleker.Size = New System.Drawing.Size(156, 46)
+        Me.btnMunkaidoleker.TabIndex = 6
+        Me.btnMunkaidoleker.Text = "Munkaidő lekérdezése"
+        Me.btnMunkaidoleker.UseVisualStyleBackColor = True
+        '
+        'btnMentes
+        '
+        Me.btnMentes.Location = New System.Drawing.Point(12, 12)
+        Me.btnMentes.Name = "btnMentes"
+        Me.btnMentes.Size = New System.Drawing.Size(75, 23)
+        Me.btnMentes.TabIndex = 7
+        Me.btnMentes.Text = "Mentés"
+        Me.btnMentes.UseVisualStyleBackColor = True
+        '
+        'btnTorles
+        '
+        Me.btnTorles.Location = New System.Drawing.Point(93, 12)
+        Me.btnTorles.Name = "btnTorles"
+        Me.btnTorles.Size = New System.Drawing.Size(75, 23)
+        Me.btnTorles.TabIndex = 8
+        Me.btnTorles.Text = "Törlés"
+        Me.btnTorles.UseVisualStyleBackColor = True
+        '
+        'btnMunkaidoossz
+        '
+        Me.btnMunkaidoossz.Location = New System.Drawing.Point(12, 41)
+        Me.btnMunkaidoossz.Name = "btnMunkaidoossz"
+        Me.btnMunkaidoossz.Size = New System.Drawing.Size(156, 23)
+        Me.btnMunkaidoossz.TabIndex = 9
+        Me.btnMunkaidoossz.Text = "Munkaidő összesítése"
+        Me.btnMunkaidoossz.UseVisualStyleBackColor = True
+        '
+        'txtMunkaidoOsszes
+        '
+        Me.txtMunkaidoOsszes.Location = New System.Drawing.Point(270, 541)
+        Me.txtMunkaidoOsszes.Name = "txtMunkaidoOsszes"
+        Me.txtMunkaidoOsszes.ReadOnly = True
+        Me.txtMunkaidoOsszes.Size = New System.Drawing.Size(166, 20)
+        Me.txtMunkaidoOsszes.TabIndex = 10
+        '
+        'lblMunkaidoOsszes
+        '
+        Me.lblMunkaidoOsszes.AutoSize = True
+        Me.lblMunkaidoOsszes.Location = New System.Drawing.Point(183, 544)
+        Me.lblMunkaidoOsszes.Name = "lblMunkaidoOsszes"
+        Me.lblMunkaidoOsszes.Size = New System.Drawing.Size(81, 13)
+        Me.lblMunkaidoOsszes.TabIndex = 11
+        Me.lblMunkaidoOsszes.Text = "Havi munkaidő:"
         '
         'wtForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(954, 538)
-        Me.Controls.Add(Me.btnMunkaidő)
+        Me.ClientSize = New System.Drawing.Size(954, 585)
+        Me.Controls.Add(Me.lblMunkaidoOsszes)
+        Me.Controls.Add(Me.txtMunkaidoOsszes)
+        Me.Controls.Add(Me.btnMunkaidoossz)
+        Me.Controls.Add(Me.btnTorles)
+        Me.Controls.Add(Me.btnMentes)
+        Me.Controls.Add(Me.btnMunkaidoleker)
         Me.Controls.Add(Me.btnFelhasznalok)
-        Me.Controls.Add(Me.dgvFelhasznalok)
+        Me.Controls.Add(Me.dgvTabla)
         Me.Controls.Add(Me.ltbFelhasznalok)
         Me.Controls.Add(Me.lblFelhasznalok)
         Me.Name = "wtForm"
         Me.Text = "Munkaidő nyilvántartó"
-        CType(Me.dgvFelhasznalok, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvTabla, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -95,7 +149,12 @@ Partial Class wtForm
 
     Friend WithEvents lblFelhasznalok As Label
     Friend WithEvents ltbFelhasznalok As ListBox
-    Friend WithEvents dgvFelhasznalok As DataGridView
+    Friend WithEvents dgvTabla As DataGridView
     Friend WithEvents btnFelhasznalok As Button
-    Friend WithEvents btnMunkaidő As Button
+    Friend WithEvents btnMunkaidoleker As Button
+    Friend WithEvents btnMentes As Button
+    Friend WithEvents btnTorles As Button
+    Friend WithEvents btnMunkaidoossz As Button
+    Friend WithEvents txtMunkaidoOsszes As TextBox
+    Friend WithEvents lblMunkaidoOsszes As Label
 End Class
--- SQL Scripts/Create_Felhasz.sql
+++ SQL Scripts/Create_Felhasz.sql
@@ -8,12 +8,13 @@ IF OBJECT_ID('dbo.Felhasznalok', 'U') IS NOT NULL
   DROP TABLE dbo.Felhasznalok
 GO
 
-CREATE TABLE dbo.Felhasznalok
-(
-	id int IDENTITY(1,1) PRIMARY KEY,
-	Nev nvarchar(255) NOT NULL, 
-	Jelszo nvarchar(255) NOT NULL,
-	Email nvarchar(255) NOT NULL,
-	Munkaido int NOT NULL
-)
+CREATE TABLE [dbo].[Felhasznalok] (
+    [id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Nev]      NVARCHAR (255) NOT NULL,
+    [Jelszo]   NVARCHAR (255) NOT NULL,
+    [Email]    NVARCHAR (255) NOT NULL,
+    [Munkaido] INT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
 GO
