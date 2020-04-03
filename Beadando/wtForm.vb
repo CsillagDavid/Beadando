a338858d41bab2deb3b611cd7da9e850bcc6c1a1
Jung Tamás <jung.tamas94@gmail.com>
2020-04-03 18:56:32 +0200

    Hónap szerinti szűrés

--- Beadando/wtForm.vb
+++ Beadando/wtForm.vb
@@ -34,34 +34,50 @@ Public Class wtForm
 
     Private Sub getRltMko() 'A napi ledolgozott órák kiszámítása, valamint a havi összesített munkaidő számolása
         Try
+            Dim cmsCount As Integer
+            Dim tryPre As Decimal
+            Dim mkoSum As Decimal
+            cmsCount = dgvTabla.Columns.Count
+            dgvTabla.Columns(cmsCount - 1).ReadOnly = False
             For index = 0 To dgvTabla.Rows.Count - 2
-                dgvTabla.Item(3, index).Value = dgvTabla.Item(2, index).Value - dgvTabla.Item(1, index).Value
+                dgvTabla.Item(cmsCount - 1, index).Value = dgvTabla.Item(cmsCount - 2, index).Value - dgvTabla.Item(cmsCount - 3, index).Value
             Next
-            Dim osszeg As Decimal
-            Dim akt As Decimal
             For index = 0 To dgvTabla.Rows.Count - 2
-                If (Decimal.TryParse(dgvTabla.Item(3, index).Value, akt)) Then
-                    osszeg += Decimal.Parse(dgvTabla.Item(3, index).Value)
+                If (Decimal.TryParse(dgvTabla.Item(cmsCount - 1, index).Value, tryPre)) Then
+                    mkoSum += Decimal.Parse(dgvTabla.Item(cmsCount - 1, index).Value)
                 End If
             Next
-            txtMunkaidoOsszes.Text = osszeg & " óra"
+            txtMunkaidoOsszes.Text = mkoSum & " óra"
+            dgvTabla.Columns(cmsCount - 1).ReadOnly = True
         Catch ex As Exception
             Console.WriteLine("A munkaidő kiszámításában hiba lépett fel!")
         End Try
     End Sub
 
     Private Sub getFszhMko(email As String) 'A kiválasztott felhasználó adott havi munkaidejének a lekérdezése
-        Dim ct As Integer
-        dgvTabla.DataSource = sqlCmd("SELECT Datum, Kezdo_ido, Befejezo_ido FROM Munkaidok M
+        If chxAkt.Checked Then
+            Dim yr, mh As String
+            yr = DateTime.Now.ToString("yyyy/MM")
+            Dim newDate As DateTime = DateTime.Now.AddMonths(1)
+            mh = newDate.ToString("yyyy/MM")
+            dgvTabla.DataSource = sqlCmd("SELECT M.id, Datum, Kezdo_ido, Befejezo_ido FROM Munkaidok M
                             INNER JOIN Felhasznalok F
                             ON M.FelhasznaloID = F.id
-                            WHERE F.Email = " & "'" & email & "'")
-        dgvTabla.Columns(0).HeaderText = "Dátum"
-        dgvTabla.Columns(1).HeaderText = "Kezdés"
-        dgvTabla.Columns(2).HeaderText = "Befejezés"
+                            WHERE F.Email = '" & email & "' AND M.Datum >= '" & yr & ". 01' AND M.Datum <= '" & mh & ". 01'")
+        Else
+            dgvTabla.DataSource = sqlCmd("SELECT M.id, Datum, Kezdo_ido, Befejezo_ido FROM Munkaidok M
+                            INNER JOIN Felhasznalok F
+                            ON M.FelhasznaloID = F.id
+                            WHERE F.Email = '" & email & "'")
+        End If
+        dgvTabla.Columns(0).Visible = False
+        dgvTabla.Columns(1).HeaderText = "Dátum"
+        dgvTabla.Columns(2).HeaderText = "Kezdés"
+        dgvTabla.Columns(3).HeaderText = "Befejezés"
         dgvTabla.Columns.Add("Napi_ido", "Napi munkaidő")
-        dgvTabla.Columns(3).ReadOnly = True
         getRltMko()
+        dgvTabla.Columns(1).ReadOnly = True
+        dgvTabla.Columns(4).ReadOnly = True
     End Sub
 
     Private Sub getFszh() 'A felhasználók adatainak lekérdezése az SQL Adatbázisból
@@ -103,22 +119,27 @@ Public Class wtForm
         cmd = con.CreateCommand()
         cmd.CommandType = CommandType.StoredProcedure
         cmd.CommandText = "saveOrUpdateMunkaidok"
-        cmd.Parameters.AddWithValue("date", Cells.Item(0).Value)
-        cmd.Parameters.AddWithValue("beginTime", Cells.Item(1).Value)
-        cmd.Parameters.AddWithValue("endTime", Cells.Item(2).Value)
+        cmd.Parameters.AddWithValue("date", Cells.Item(1).Value)
+        cmd.Parameters.AddWithValue("beginTime", Cells.Item(2).Value)
+        cmd.Parameters.AddWithValue("endTime", Cells.Item(3).Value)
         cmd.ExecuteNonQuery()
         sqlConnection.sqlClose()
-        MsgBox("Edited: " & Cells.Item(0).Value & " " & Cells.Item(1).Value & " " & Cells.Item(2).Value & " " & Cells.Item(3).Value)
+        MsgBox("Edited: " & Cells.Item(1).Value & " " & Cells.Item(2).Value & " " & Cells.Item(3).Value & " " & Cells.Item(4).Value)
     End Sub
 
     Private Sub dgvTabla_CellEndEdit(sender As Object, e As DataGridViewCellEventArgs) Handles dgvTabla.CellEndEdit
-        If Not dgvTabla.Rows.Item(e.RowIndex).Cells.Item(e.ColumnIndex).Value.Equals(dgvTabla.Rows.Item(e.RowIndex).Tag) Then
-            If Not editedRows.Contains(e.RowIndex) Then
-                editedRows.Add(e.RowIndex)
+        Try
+            If Not dgvTabla.Rows.Item(e.RowIndex).Cells.Item(e.ColumnIndex).Value.Equals(dgvTabla.Rows.Item(e.RowIndex).Tag) Then
+                If Not editedRows.Contains(e.RowIndex) Then
+                    editedRows.Add(e.RowIndex)
+                End If
+                getRltMko()
             End If
-            getRltMko()
-        End If
-        dgvTabla.Rows.Item(e.RowIndex).Tag = ""
+            dgvTabla.Rows.Item(e.RowIndex).Tag = ""
+        Catch ex As Exception
+            Console.WriteLine("Hiba a cellamódosításban.")
+        End Try
+
     End Sub
 
     Private Sub btnMunkaidoleker_Click(sender As Object, e As EventArgs) Handles btnMunkaidoleker.Click
@@ -127,6 +148,20 @@ Public Class wtForm
 
     Private Sub dgvTabla_CellBeginEdit(sender As Object, e As DataGridViewCellCancelEventArgs) Handles dgvTabla.CellBeginEdit
         dgvTabla.Rows.Item(e.RowIndex).Tag = dgvTabla.Rows.Item(e.RowIndex).Cells.Item(e.ColumnIndex).Value
+        getDate()
     End Sub
 
+    Private Sub getDate()
+        Dim cmsCount As Integer
+        Dim rowCount As Integer
+        cmsCount = dgvTabla.Columns.Count
+        rowCount = dgvTabla.Rows.Count
+        dgvTabla.Columns(cmsCount - 4).ReadOnly = False
+        dgvTabla.Item(cmsCount - 4, rowCount - 1).Value = DateTime.Now.ToString("yyyy/MM/dd") & "."
+        dgvTabla.Columns(cmsCount - 4).ReadOnly = True
+    End Sub
+
+    Private Sub btnMunkaidoossz_Click(sender As Object, e As EventArgs) Handles btnMunkaidoossz.Click
+
+    End Sub
 End Class
