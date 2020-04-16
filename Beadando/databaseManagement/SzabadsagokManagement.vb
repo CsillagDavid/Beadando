b03f1ccd6e09f6b991eb5b538e63f26089faf235
Csillag Dávid <dvid.csillag@gmail.com>
2020-04-16 21:21:17 +0200

    SzabadsagokManagement commit

--- Beadando/databaseManagement/SzabadsagokManagement.vb
+++ Beadando/databaseManagement/SzabadsagokManagement.vb
@@ -45,6 +45,23 @@ Public Class SzabadsagokManagement
         Return lista
     End Function
 
-
+    Public Function GetByEmailAndDate(Email As String, KezdoDatum As String, BefejezoDatum As String) As List(Of Szabadsagok)
+        Dim lista As New List(Of Szabadsagok)
+        sqlConnection.sqlConnect()
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "SELECT SZ.id, SZ.Datum, SZ.Tipus, SZ.FelhasznaloID
+                            FROM Szabadsagok SZ INNER JOIN Felhasznalok F ON SZ.FelhasznaloID = F.id
+                            WHERE F.Email='" & Email & "' AND M.Datum >= '" & KezdoDatum & ". " & BefejezoDatum & ". 01' 
+                                AND M.Datum < '" & KezdoDatum & ". " & (BefejezoDatum + 1) & ". 01'"
+        Dim reader As SqlDataReader
+        reader = cmd.ExecuteReader()
+        While reader.Read()
+            Dim szabadsag = New Szabadsagok(reader.GetInt32(0), reader.GetDateTime(1), reader.GetInt32(2), reader.GetInt32(3))
+            lista.Add(szabadsag)
+        End While
+        sqlConnection.sqlClose()
+        Return lista
+    End Function
 
 End Class
