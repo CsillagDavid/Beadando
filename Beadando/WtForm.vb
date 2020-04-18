8a77e493f907b9720717426896f2a6e224d94d6a
Jung Tamás <jung.tamas94@gmail.com>
2020-04-18 08:39:30 +0200

    buuuuug

--- Beadando/WtForm.vb
+++ Beadando/WtForm.vb
@@ -10,9 +10,9 @@ Public Class WtForm
     Dim fhszMan As New FelhasznalokManagement
     Dim unnepMan As New UnnepnapokManagement
     Dim jgkkMan As New JogkorokManagement
-    Dim szabMan As New SzabadsagokManagement
 
     'SQL kapcsolathoz szükséges változók
+    Dim szabMan As New SzabadsagokManagement
     Dim con As New SqlConnection
     Dim cmd As New SqlCommand
     Dim sqlConnection As SqlConn
@@ -62,10 +62,6 @@ Public Class WtForm
                 LblJogkor.Enabled = True
                 BtnJogkor.Visible = True
                 BtnJogkor.Enabled = True
-                LblGeneralas.Visible = True
-                LblGeneralas.Enabled = True
-                BtnGeneralas.Visible = True
-                BtnGeneralas.Enabled = True
             Case "Vezeto"
                 GetFszhLtb()
                 SetDefaultWorkingHours(User.email)
@@ -115,7 +111,13 @@ Public Class WtForm
             befejezo = IsDecimal(tabla.Item("befejezo_ido", index).Value)
             kezdo = IsDecimal(tabla.Item("kezdo_ido", index).Value)
             mkoSum = IsDecimal(TxtMunkaidoOsszes.Text)
-            tabla.Item("napi_ido", index).Value = befejezo - kezdo
+            If tabla.Item("tavollet", index).Value = "Szabadság" Then
+                tabla.Item("napi_ido", index).Value = 0
+                tabla.Item("befejezo_ido", index).Value = 0
+                tabla.Item("kezdo_ido", index).Value = 0
+            Else
+                tabla.Item("napi_ido", index).Value = befejezo - kezdo
+            End If
             mkoSum += (befejezo - kezdo)
             TxtMunkaidoOsszes.Visible = True
             LblMunkaidoOsszes.Visible = True
@@ -235,11 +237,14 @@ Public Class WtForm
         BtnMentes.Enabled = True
         BtnTorles.Enabled = False
 
-        If User.role = "Beosztott" Then
-            DgvTabla.AllowUserToAddRows = False
-        Else
-            DgvTabla.AllowUserToAddRows = True
-        End If
+        Select Case User.role
+            Case "Admin"
+                DgvTabla.AllowUserToAddRows = True
+            Case "Vezeto"
+                DgvTabla.AllowUserToAddRows = True
+            Case "Beosztott"
+                DgvTabla.AllowUserToAddRows = False
+        End Select
 
     End Sub
 
@@ -409,17 +414,24 @@ Public Class WtForm
         Dim evhonap = GetYearAndMonth()
         Dim felhaszMunkaido As New List(Of List(Of Munkaidok))
 
-        If User.Role = "Beosztott" Then
-            Dim ujmunkaido As New List(Of Munkaidok)
-            mkidoMan.GetMunkaidok(ujmunkaido, userEmail, evhonap.item(itemEv), evhonap.item(itemHonap))
-            felhaszMunkaido.Add(ujmunkaido)
-        Else
-            For index = 0 To fhszLista.Count - 1
+        Select Case User.role
+            Case "Admin"
+                For index = 0 To fhszLista.Count - 1
+                    Dim ujmunkaido As New List(Of Munkaidok)
+                    mkidoMan.GetMunkaidok(ujmunkaido, fhszLista(index).Email, evhonap.item(itemEv), evhonap.item(itemHonap))
+                    felhaszMunkaido.Add(ujmunkaido)
+                Next
+            Case "Vezeto"
+                For index = 0 To fhszLista.Count - 1
+                    Dim ujmunkaido As New List(Of Munkaidok)
+                    mkidoMan.GetMunkaidok(ujmunkaido, fhszLista(index).Email, evhonap.item(itemEv), evhonap.item(itemHonap))
+                    felhaszMunkaido.Add(ujmunkaido)
+                Next
+            Case "Beosztott"
                 Dim ujmunkaido As New List(Of Munkaidok)
-                mkidoMan.GetMunkaidok(ujmunkaido, fhszLista(index).Email, evhonap.item(itemEv), evhonap.item(itemHonap))
+                mkidoMan.GetMunkaidok(ujmunkaido, userEmail, evhonap.item(itemEv), evhonap.item(itemHonap))
                 felhaszMunkaido.Add(ujmunkaido)
-            Next
-        End If
+        End Select
 
         DgvUj.Columns.Add("Nev", "Név")
         DgvUj.Columns.Add("Email", "E-mail")
@@ -630,16 +642,31 @@ Public Class WtForm
 
     'Felhasználó munkaidejének lekérdezése
     Private Sub BtnMunkaidoleker_Click(sender As Object, e As EventArgs) Handles BtnMunkaidoleker.Click
-        If User.Role = "Beosztott" Then
-            GetUserWorkingHours(userEmail)
-        Else
-            GetUserWorkingHours(LtbFelhasznalok.SelectedValue)
-        End If
+        Select Case User.role
+            Case "Admin"
+                GetUserWorkingHours(LtbFelhasznalok.SelectedValue)
+            Case "Vezeto"
+                GetUserWorkingHours(LtbFelhasznalok.SelectedValue)
+            Case "Beosztott"
+                GetUserWorkingHours(userEmail)
+        End Select
     End Sub
 
     'Munkaidő összesítés gomb
     Private Sub BtnMunkaidoossz_Click(sender As Object, e As EventArgs) Handles BtnMunkaidoossz.Click
         GetWorkingHoursSummary()
+    End Sub
+
+    'Teszt gomb, később törlésre kerül
+    Private Sub TstButton_Click(sender As Object, e As EventArgs) Handles TstButton.Click
+        Select Case User.role
+            Case "Admin"
+                SetDefaultWorkingHours(LtbFelhasznalok.SelectedValue)
+            Case "Vezeto"
+                SetDefaultWorkingHours(LtbFelhasznalok.SelectedValue)
+            Case "Beosztott"
+                SetDefaultWorkingHours(userEmail)
+        End Select
     End Sub
 
     'Törlés gomb működtetése
@@ -674,19 +701,14 @@ Public Class WtForm
         End If
     End Sub
 
-    'Ünnepnapok lekérdezése és szerkesztése, csak adminként működik
+    'Ünnepnapok lekérdezése és szerkesztése
     Private Sub BtnUnnep_Click(sender As Object, e As EventArgs) Handles BtnUnnep.Click
         SetHoliday()
     End Sub
 
-    'Jogkörök lekérdezése és szerkesztése, csak adminként működik
+    'Jogkörök lekérdezése és szerkesztése
     Private Sub BtnJogkor_Click(sender As Object, e As EventArgs) Handles BtnJogkor.Click
         GetJogkorok()
-    End Sub
-
-    'Alapértelmezett munkaidő generálása gomb álltal, csak adminként működik
-    Private Sub BtnGeneralas_Click(sender As Object, e As EventArgs) Handles BtnGeneralas.Click
-        SetDefaultWorkingHours(LtbFelhasznalok.SelectedValue)
     End Sub
 
 #End Region
